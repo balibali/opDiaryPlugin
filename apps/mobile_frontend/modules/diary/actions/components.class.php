df6ab0cbdbffd9b00bf8ae27de0b8cddcf31d54f
ogawa <ogawa@8bbc783d-d51a-0410-adab-8fa69d43f207>
2008-12-24 06:25:28 +0000

    #3209:added components to the mobile_frontend app

---
+++ apps/mobile_frontend/modules/diary/actions/components.class.php
@@ -0,0 +1,12 @@
+<?php
+
+/**
+ * diary components.
+ *
+ * @package    OpenPNE
+ * @subpackage diary
+ * @author     Rimpei Ogawa <ogawa@tejimaya.com>
+ */
+class diaryComponents extends opDiaryPluginDiaryComponents
+{
+}
