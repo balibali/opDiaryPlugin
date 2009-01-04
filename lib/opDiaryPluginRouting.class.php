133c4c06ea8ef8347addefd318604f5cf75940be
ogawa <ogawa@8bbc783d-d51a-0410-adab-8fa69d43f207>
2009-01-04 13:29:03 +0000

    changed routing class to put in order of priority the routing rules

--- lib/opDiaryPluginRouting.class.php
+++ lib/opDiaryPluginRouting.class.php
@@ -11,114 +11,97 @@ class opDiaryPluginRouting
   static public function listenToRoutingLoadConfigurationEvent(sfEvent $event)
   {
     $routing = $event->getSubject();
-    $routing->prependRoute('diary_delete_default',
-      new sfRoute(
-        '/diary/*',
-        array('module' => 'default', 'action' => 'error')
-      )
-    );
-    $routing->prependRoute('diary_index',
-      new sfRoute(
+
+    $routes = array(
+      'diary_index' => new sfRoute(
         '/diary',
         array('module' => 'diary', 'action' => 'index')
-      )
-    );
-    $routing->prependRoute('diary_list',
-      new sfRoute(
+      ),
+
+      'diary_list' => new sfRoute(
         '/diary/list',
         array('module' => 'diary', 'action' => 'list')
-      )
-    );
-    $routing->prependRoute('diary_list_member',
-      new sfPropelRoute(
+      ),
+      'diary_list_member' => new sfPropelRoute(
         '/diary/listMember/:id',
         array('module' => 'diary', 'action' => 'listMember'),
         array('id' => '\d+'),
         array('model' => 'Member', 'type' => 'object')
-      )
-    );
-    $routing->prependRoute('diary_list_friend',
-      new sfRoute(
+      ),
+      'diary_list_friend' => new sfRoute(
         '/diary/listFriend',
         array('module' => 'diary', 'action' => 'listFriend')
-      )
-    );
-    $routing->prependRoute('diary_show',
-      new sfPropelRoute(
+      ),
+      'diary_show' => new sfPropelRoute(
         '/diary/:id',
         array('module' => 'diary', 'action' => 'show'),
         array('id' => '\d+'),
         array('model' => 'Diary', 'type' => 'object')
-      )
-    );
-    $routing->prependRoute('diary_new',
-      new sfRoute(
+      ),
+
+      'diary_new' => new sfRoute(
         '/diary/new',
         array('module' => 'diary', 'action' => 'new')
-      )
-    );
-    $routing->prependRoute('diary_create',
-      new sfRoute(
+      ),
+      'diary_create' => new sfRoute(
         '/diary/create',
         array('module' => 'diary', 'action' => 'create'),
         array('sf_method' => array('post'))
-      )
-    );
-    $routing->prependRoute('diary_edit',
-      new sfPropelRoute(
+      ),
+      'diary_edit' => new sfPropelRoute(
         '/diary/edit/:id',
         array('module' => 'diary', 'action' => 'edit'),
         array('id' => '\d+'),
         array('model' => 'Diary', 'type' => 'object')
-      )
-    );
-    $routing->prependRoute('diary_update',
-      new sfPropelRoute(
+      ),
+      'diary_update' => new sfPropelRoute(
         '/diary/update/:id',
         array('module' => 'diary', 'action' => 'update'),
         array('id' => '\d+', 'sf_method' => array('post')),
         array('model' => 'Diary', 'type' => 'object')
-      )
-    );
-    $routing->prependRoute('diary_delete_confirm',
-      new sfPropelRoute(
+      ),
+      'diary_delete_confirm' => new sfPropelRoute(
         '/diary/deleteConfirm/:id',
         array('module' => 'diary', 'action' => 'deleteConfirm'),
         array('id' => '\d+'),
         array('model' => 'Diary', 'type' => 'object')
-      )
-    );
-    $routing->prependRoute('diary_delete',
-      new sfPropelRoute(
+      ),
+      'diary_delete' => new sfPropelRoute(
         '/diary/delete/:id',
         array('module' => 'diary', 'action' => 'delete'),
         array('id' => '\d+', 'sf_method' => array('post')),
         array('model' => 'Diary', 'type' => 'object')
-      )
-    );
-    $routing->prependRoute('diary_comment_create',
-      new sfPropelRoute(
+      ),
+
+      'diary_comment_create' => new sfPropelRoute(
         '/diary/:id/comment/create',
         array('module' => 'diaryComment', 'action' => 'create'),
         array('id' => '\d+', 'sf_method' => array('post')),
         array('model' => 'Diary', 'type' => 'object')
-      )
-    );
-    $routing->prependRoute('diary_comment_delete_confirm',
-      new sfPropelRoute(
+      ),
+      'diary_comment_delete_confirm' => new sfPropelRoute(
         '/diary/comment/deleteConfirm/:id',
         array('module' => 'diaryComment', 'action' => 'deleteConfirm'),
         array('id' => '\d+'),
         array('model' => 'DiaryComment', 'type' => 'object')
-      )
-    );
-    $routing->prependRoute('diary_comment_delete',
-      new sfPropelRoute(
+      ),
+      'diary_comment_delete' => new sfPropelRoute(
         '/diary/comment/delete/:id',
         array('module' => 'diaryComment', 'action' => 'delete'),
         array('id' => '\d+', 'sf_method' => array('post')),
         array('model' => 'DiaryComment', 'type' => 'object')
-      )
+      ),
+
+      'diary_nodefaults' => new sfRoute(
+        '/diary/*',
+        array('module' => 'default', 'action' => 'error')
+      ),
     );
+
+    $routes = array_reverse($routes);
+    foreach ($routes as $name => $route)
+    {
+      $routing->prependRoute($name, $route);
+    }
   }
 }
