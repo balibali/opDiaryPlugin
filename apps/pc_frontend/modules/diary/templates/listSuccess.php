036cad805cbec2373a4243320599f127e0ecbb59
ogawa <ogawa@8bbc783d-d51a-0410-adab-8fa69d43f207>
2009-01-04 05:26:37 +0000

    #3209:enabled images to link to a diary page in the list

--- apps/pc_frontend/modules/diary/templates/listSuccess.php
+++ apps/pc_frontend/modules/diary/templates/listSuccess.php
@@ -7,7 +7,7 @@
 <div class="block">
 <?php foreach ($pager->getResults() as $diary): ?>
 <div class="ditem"><div class="item"><table><tbody><tr>
-<td rowspan="4" class="photo"><?php echo image_tag_sf_image($diary->getMember()->getImageFilename(), array('size' => '76x76')) ?></td>
+<td rowspan="4" class="photo"><a href="<?php echo url_for('diary_show', $diary) ?>"><?php echo image_tag_sf_image($diary->getMember()->getImageFilename(), array('size' => '76x76')) ?></a></td>
 <th><?php echo __('Nickname') ?></th><td><?php echo $diary->getMember()->getName() ?></td>
 </tr><tr>
 <th><?php echo __('Title') ?></th><td><?php echo $diary->getTitle() ?></td>
