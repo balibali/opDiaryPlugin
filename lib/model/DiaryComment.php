577eb6f79cd1b576504321026a44d44232d09f23
ogawa <ogawa@8bbc783d-d51a-0410-adab-8fa69d43f207>
2009-01-04 05:14:38 +0000

    #3209:added number to diary comments

--- lib/model/DiaryComment.php
+++ lib/model/DiaryComment.php
@@ -2,4 +2,13 @@
 
 class DiaryComment extends BaseDiaryComment
 {
+  public function save(PropelPDO $con = null)
+  {
+    if ($this->isNew() && !$this->getNumber())
+    {
+      $this->setNumber(DiaryCommentPeer::getMaxNumber($this->getDiaryId()) + 1);
+    }
+
+    parent::save($con);
+  }
 }
--- lib/model/DiaryCommentPeer.php
+++ lib/model/DiaryCommentPeer.php
@@ -2,4 +2,17 @@
 
 class DiaryCommentPeer extends BaseDiaryCommentPeer
 {
+  public static function getMaxNumber($diaryId)
+  {
+    $c = new Criteria();
+    $c->clearSelectColumns()->addSelectColumn(self::NUMBER);
+    $c->add(self::DIARY_ID, $diaryId);
+    $c->addDescendingOrderByColumn(self::NUMBER);
+    $c->setLimit(1);
+
+    $stmt = self::doSelectStmt($c);
+    $row = $stmt->fetch(PDO::FETCH_NUM);
+
+    return (int)$row[0];
+  }
 }
