432497c1ba74ff128999a46094f3e57e1385dfc2
ogawa <ogawa@8bbc783d-d51a-0410-adab-8fa69d43f207>
2009-01-05 15:16:03 +0000

    #3209:removed unused comment from a template

--- apps/mobile_frontend/modules/diary/templates/showSuccess.php
+++ apps/mobile_frontend/modules/diary/templates/showSuccess.php
@@ -14,32 +14,6 @@
 
 (<?php echo $diary->getPublicFlagLabel() ?>)<br>
 
-<?php /*
-<?php if ($pager->getNbResults()): ?>
-<hr>
-<center>
-<?php echo __('Comments') ?><br>
-<?php echo pager_total($pager); ?><br>
-</center>
-
-<?php $comments = $pager->getResults(); ?>
-<?php foreach ($comments as $comment): ?>
-<hr>
-▼[<?php printf('%03d', $comment->getNumber()) ?>]<?php echo op_diary_format_date($comment->getCreatedAt(), 'XDateTime') ?>
-<?php if ($diary->getMemberId() === $sf_user->getMemberId() || $comment->getMemberId() === $sf_user->getMemberId()): ?>
-[<?php echo link_to(__('Delete'), 'diary_comment_delete_confirm', $comment) ?>]
-<?php endif; ?><br>
-<?php echo link_to($comment->getMember()->getName(), 'member/show?id='.$comment->getMemberId()) ?><br>
-<?php echo nl2br($comment->getBody()) ?><br>
-<?php foreach ($comment->getDiaryCommentImages() as $image): ?>
-<?php echo link_to(__('View Image'), sf_image_path($image->getFile(), array('size' => '240x320', 'f' => 'jpg'))) ?><br>
-<?php endforeach; ?>
-<?php endforeach; ?>
-
-<?php echo pager_navigation($pager, '@diary_show?page=%d&id='.$diary->getId(), false) ?>
-<?php endif; ?>
-*/ ?>
-
 <?php include_component('diaryComment', 'list', array('diary' => $diary)) ?>
 
 <hr>
