5bd1a55a576ccd4ddd76c8840259f661f01654ac
ebihara <ebihara@8bbc783d-d51a-0410-adab-8fa69d43f207>
2009-01-07 05:04:17 +0000

    #3385:added the LICENSE file is a copy of the Apache License 2.0 and reference to it

--- lib/helper/opDiaryHelper.php
+++ lib/helper/opDiaryHelper.php
@@ -1,5 +1,13 @@
 <?php
 
+/**
+ * This file is part of the OpenPNE package.
+ * (c) OpenPNE Project (http://www.openpne.jp/)
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file and the NOTICE file that were distributed with this source code.
+ */
+
 use_helper('Date');
 
 function op_diary_format_date($date, $format = 'd', $culture = null, $charset = null)
