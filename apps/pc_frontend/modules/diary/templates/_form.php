898dcfc8e2e4eff5e930b7af6b4b037d25549a5f
ogawa <ogawa@8bbc783d-d51a-0410-adab-8fa69d43f207>
2009-01-20 10:18:41 +0000

    fixed diary form design

--- apps/pc_frontend/modules/diary/templates/_form.php
+++ apps/pc_frontend/modules/diary/templates/_form.php
@@ -4,21 +4,20 @@ $form->getWidget('body')->setAttribute('rows', 10);
 $form->getWidget('body')->setAttribute('cols', 50);
 
 $options = array(
-  'form' => $form,
   'button' => __('Save'),
   'isMultipart' => true,
 );
 
 if ($form->isNew())
 {
-  $title = __('Post a diary');
+  $options['title'] = __('Post a diary');
   $options['url'] = 'diary/create';
 }
 else
 {
-  $title = __('Edit the diary');
+  $options['title'] = __('Edit the diary');
   $options['url'] = 'diary/update?id='.$diary->getId();
 }
 
-include_box('formDiary', $title, '', $options);
+op_include_parts('form', 'diaryForm', $form, $options);
 ?>
