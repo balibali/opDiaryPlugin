b45a4dc5e976d334b4398184b55a81212f9c3261
ebihara <ebihara@8bbc783d-d51a-0410-adab-8fa69d43f207>
2009-01-26 11:48:41 +0000

    changed op_diary_format_date() to call op_format_date()

--- lib/helper/opDiaryHelper.php
+++ lib/helper/opDiaryHelper.php
@@ -8,86 +8,12 @@
  * file and the NOTICE file that were distributed with this source code.
  */
 
+use_helper('opUtil');
 use_helper('Date');
 
 function op_diary_format_date($date, $format = 'd', $culture = null, $charset = null)
 {
-  if (!$culture)
-  {
-    $culture = sfContext::getInstance()->getUser()->getCulture();
-  }
-
-  switch ($format)
-  {
-    case 'XShortDate':
-      switch ($culture)
-      {
-        case 'ja_JP':
-          $format = 'MM/dd';
-          break;
-        default:
-          $format = 'd';
-          break;
-      }
-      break;
-    case 'XShortDateJa':
-      switch ($culture)
-      {
-        case 'ja_JP':
-          $format = 'MM月dd日';
-          break;
-        default:
-          $format = 'd';
-          break;
-      }
-      break;
-    case 'XDateTime':
-      switch ($culture)
-      {
-        case 'ja_JP':
-          $format = 'yyyy/MM/dd HH:mm';
-          break;
-        default:
-          $format = 'f';
-          break;
-      }
-      break;
-    case 'XDateTimeJa':
-      switch ($culture)
-      {
-        case 'ja_JP':
-          $format = 'yyyy年MM月dd日 HH:mm';
-          break;
-        default:
-          $format = 'f';
-          break;
-      }
-      break;
-    case 'XDateTimeJaBr':
-      switch ($culture)
-      {
-        case 'ja_JP':
-          $format = "yyyy年\nMM月dd日\nHH:mm";
-          break;
-        default:
-          $format = 'f';
-          break;
-      }
-      break;
-    case 'XCalendarMonth':
-      switch ($culture)
-      {
-        case 'ja_JP':
-          $format = 'yyyy年M月';
-          break;
-        default:
-          $format = 'MMMM yyyy';
-          break;
-      }
-      break;
-  }
-
-  return format_date($date, $format, $culture, $charset);
+  return op_format_date($date, $format, $culture, $charset);
 }
 
 function op_diary_within_page_link($marker = '▼')
