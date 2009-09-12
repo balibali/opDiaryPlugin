e3db101590a3b7eedfab159367fc9215bb797540
Rimpei Ogawa <ogawa@tejimaya.com>
2009-09-13 05:37:28 +0900

    fixed duplicate pagers

--- apps/pc_frontend/modules/diary/templates/listFriendSuccess.php
+++ apps/pc_frontend/modules/diary/templates/listFriendSuccess.php
@@ -4,14 +4,14 @@
 <?php if ($pager->getNbResults()): ?>
 <div class="dparts recentList"><div class="parts">
 <div class="partsHeading"><h3><?php echo $title ?></h3></div>
-<div class="pagerRelative"><p class="number"><?php echo op_include_pager_navigation($pager, 'diary/listFriend?page=%d'); ?></p></div>
+<?php echo op_include_pager_navigation($pager, 'diary/listFriend?page=%d'); ?>
 <?php foreach ($pager->getResults() as $diary): ?>
 <dl>
 <dt><?php echo op_format_date($diary->getCreatedAt(), 'XDateTimeJa') ?></dt>
 <dd><?php echo link_to(op_diary_get_title_and_count($diary), 'diary_show', $diary) ?> (<?php echo $diary->getMember()->getName() ?>)<?php if ($diary->hasImages()) : ?> <?php echo image_tag('icon_camera.gif', array('alt' => 'photo')) ?><?php endif; ?></dd>
 </dl>
 <?php endforeach; ?>
-<div class="pagerRelative"><p class="number"><?php echo op_include_pager_navigation($pager, 'diary/listFriend?page=%d'); ?></p></div>
+<?php echo op_include_pager_navigation($pager, 'diary/listFriend?page=%d'); ?>
 </div></div>
 <?php else: ?>
 <?php op_include_box('diaryList', __('There are no diaries'), array('title' => $title)) ?>
--- apps/pc_frontend/modules/diary/templates/listMemberSuccess.php
+++ apps/pc_frontend/modules/diary/templates/listMemberSuccess.php
@@ -17,14 +17,14 @@ if ($year && $month)
 <?php if ($pager->getNbResults()): ?>
 <div class="dparts recentList"><div class="parts">
 <div class="partsHeading"><h3><?php echo $title ?></h3></div>
-<div class="pagerRelative"><p class="number"><?php echo op_include_pager_navigation($pager, 'diary/listMember?page=%d&id='.$member->getId().(($year && $month) ? '&year='.$year.'&month='.$month.(($day) ? '&day='.$day : '') : '')); ?></p></div>
+<?php echo op_include_pager_navigation($pager, 'diary/listMember?page=%d&id='.$member->getId().(($year && $month) ? '&year='.$year.'&month='.$month.(($day) ? '&day='.$day : '') : '')); ?>
 <?php foreach ($pager->getResults() as $diary): ?>
 <dl>
 <dt><?php echo op_format_date($diary->getCreatedAt(), 'XDateTimeJa') ?></dt>
 <dd><?php echo link_to(op_diary_get_title_and_count($diary), 'diary_show', $diary) ?><?php if ($diary->hasImages()) : ?> <?php echo image_tag('icon_camera.gif', array('alt' => 'photo')) ?><?php endif; ?></dd>
 </dl>
 <?php endforeach; ?>
-<div class="pagerRelative"><p class="number"><?php echo op_include_pager_navigation($pager, 'diary/listMember?page=%d&id='.$member->getId().(($year && $month) ? '&year='.$year.'&month='.$month.(($day) ? '&day='.$day : '') : '')); ?></p></div>
+<?php echo op_include_pager_navigation($pager, 'diary/listMember?page=%d&id='.$member->getId().(($year && $month) ? '&year='.$year.'&month='.$month.(($day) ? '&day='.$day : '') : '')); ?>
 </div></div>
 <?php else: ?>
 <?php op_include_box('diaryList', __('There are no diaries'), array('title' => $title)) ?>
