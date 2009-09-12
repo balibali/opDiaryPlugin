40144911e320d6d4bd3753c8945c29f17451828a
Rimpei Ogawa <ogawa@tejimaya.com>
2009-09-13 04:40:05 +0900

    added pager test for diary/list

--- test/functional/pc_frontend/diaryActionsTest.php
+++ test/functional/pc_frontend/diaryActionsTest.php
@@ -19,3 +19,21 @@ $test->get('/diary')
     ->checkElement('h3', 'Recently Posted Diaries')
   ->end()
 ;
+
+$test->info('Diary Pager Test')
+  ->get('/diary/list')
+  ->with('response')->begin()
+    ->checkElement('.pagerRelative', true, array('count' => 2))
+    ->checkElement('.pagerRelative .number', '1 - 20 of 22')
+    ->checkElement('.pagerRelative .prev', false)
+    ->checkElement('.pagerRelative .next', true)
+  ->end()
+
+  ->get('/diary/list', array('page' => 2))
+  ->with('response')->begin()
+    ->checkElement('.pagerRelative', true, array('count' => 2))
+    ->checkElement('.pagerRelative .number', '21 - 22 of 22')
+    ->checkElement('.pagerRelative .prev', true)
+    ->checkElement('.pagerRelative .next', false)
+  ->end()
+;
