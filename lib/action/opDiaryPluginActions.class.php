f5b6c4a2b7debd1ddd89725fd203d95bf205a0ac
Rimpei Ogawa <ogawa@tejimaya.com>
2009-10-22 11:53:13 +0900

    moved default security settings to opDiaryPluginAction (fixes #115)

--- lib/action/opDiaryPluginActions.class.php
+++ lib/action/opDiaryPluginActions.class.php
@@ -17,6 +17,13 @@
  */
 class opDiaryPluginActions extends sfActions
 {
+  public function initialize($context, $moduleName, $actionName)
+  {
+    parent::initialize($context, $moduleName, $actionName);
+
+    $this->security['all'] = array('is_secure' => true, 'credentials' => 'SNSMember');
+  }
+
   public function preExecute()
   {
     if (is_callable(array($this->getRoute(), 'getObject')))
