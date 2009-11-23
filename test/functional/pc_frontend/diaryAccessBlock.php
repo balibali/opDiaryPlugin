61f0d9a71f3f1d0a558327f469b43ff8294e339c
Rimpei Ogawa <ogawa@tejimaya.com>
2009-11-23 23:01:37 +0900

    added functional tests for the feature of access block (refs #251)

---
+++ test/functional/pc_frontend/diaryAccessBlock.php
@@ -0,0 +1,59 @@
+<?php
+
+include dirname(__FILE__).'/../../bootstrap/functional.php';
+
+$test = new opTestFunctional(new sfBrowser(), new lime_test(null, new lime_output_color()));
+
+include dirname(__FILE__).'/../../bootstrap/database.php';
+
+$test->setCulture('en');
+
+$test->info('Unauthenticated User Test')
+  ->get('/diary/listMember/1/2009/3/1')
+  ->isForwardedTo('diary', 'listMember')
+
+  ->get('/diary/listMember/1/2009/3')
+  ->isForwardedTo('diary', 'listMember')
+
+  ->get('/diary/listMember/1')
+  ->isForwardedTo('diary', 'listMember')
+
+  ->get('/diary/3')
+  ->isForwardedTo('diary', 'show')
+;
+
+$test->login('sns4@example.com', 'password');
+$test->info('AccessBlocked Member Test')
+  ->get('/diary/listMember/1/2009/3/1')
+  ->isForwardedTo('default', 'error')
+
+  ->get('/diary/listMember/2/2009/3/1')
+  ->isForwardedTo('diary', 'listMember')
+
+  ->get('/diary/listMember/1/2009/3')
+  ->isForwardedTo('default', 'error')
+
+  ->get('/diary/listMember/2/2009/3')
+  ->isForwardedTo('diary', 'listMember')
+
+  ->get('/diary/listMember/1')
+  ->isForwardedTo('default', 'error')
+
+  ->get('/diary/listMember/2')
+  ->isForwardedTo('diary', 'listMember')
+
+  ->get('/diary/listMember')
+  ->isForwardedTo('diary', 'listMember')
+
+  ->get('/diary/1')
+  ->isForwardedTo('default', 'error')
+
+  ->get('/diary/2')
+  ->isForwardedTo('diary', 'show')
+
+  ->post('/diary/1/comment/create')
+  ->isForwardedTo('default', 'error')
+
+  ->post('/diary/2/comment/create')
+  ->isForwardedTo('diaryComment', 'create')
+;
