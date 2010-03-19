3a32caea72d79840a5a376fb66056b7de4e88f12
Kiwa Sakai <sakai@tejimaya.com>
2010-03-19 11:45:59 +0900

    use _partsList for diaryCommentList (refs #878)

---
+++ apps/mobile_frontend/modules/diaryComment/templates/_comment.php
@@ -0,0 +1,12 @@
+<?php echo op_within_page_link() ?>
+[<?php printf('%03d', $comment->number) ?>]<?php echo op_format_date($comment->created_at, 'XDateTime') ?>
+<?php if ($diary->member_id === $sf_user->getMemberId() || $comment->member_id === $sf_user->getMemberId()): ?>
+[<?php echo link_to(__('Delete'), 'diary_comment_delete_confirm', $comment) ?>]
+<?php endif; ?><br>
+<?php echo link_to($comment->Member->name, 'member/profile?id='.$comment->member_id) ?><br>
+<?php echo nl2br($comment->body) ?><br>
+<?php if ($comment->has_images): ?>
+<?php foreach ($comment->getDiaryCommentImagesJoinFile() as $image): ?>
+<?php echo link_to(__('View Image'), sf_image_path($image->File, array('size' => '240x320', 'f' => 'jpg'))) ?><br>
+<?php endforeach; ?>
+<?php endif; ?>
--- apps/mobile_frontend/modules/diaryComment/templates/_list.php
+++ apps/mobile_frontend/modules/diaryComment/templates/_list.php
@@ -1,30 +1,22 @@
 <?php use_helper('opDiary') ?>
 
 <?php if ($pager->getNbResults()): ?>
-<hr>
+<hr color="<?php echo $op_color["core_color_11"] ?>">
 <center>
 <?php echo __('Comments') ?><br>
 <?php echo __('No. %1% - %2%', array('%1%' => $pager->getFirstItem()->getNumber(), '%2%' => $pager->getLastItem()->getNumber())) ?><br>
 </center>
 
-<?php foreach ($pager->getResults() as $comment): ?>
-<hr>
-<?php echo op_within_page_link() ?>
-[<?php printf('%03d', $comment->number) ?>]<?php echo op_format_date($comment->created_at, 'XDateTime') ?>
-<?php if ($diary->member_id === $sf_user->getMemberId() || $comment->member_id === $sf_user->getMemberId()): ?>
-[<?php echo link_to(__('Delete'), 'diary_comment_delete_confirm', $comment) ?>]
-<?php endif; ?><br>
-<?php echo link_to($comment->Member->name, 'member/profile?id='.$comment->member_id) ?><br>
-<?php echo nl2br($comment->body) ?><br>
-<?php if ($comment->has_images): ?>
-<?php foreach ($comment->getDiaryCommentImagesJoinFile() as $image): ?>
-<?php echo link_to(__('View Image'), sf_image_path($image->File, array('size' => '240x320', 'f' => 'jpg'))) ?><br>
-<?php endforeach; ?>
-<?php endif; ?>
-<?php endforeach; ?>
+<?php
+foreach ($pager->getResults() as $comment)
+{
+  $list[] = get_partial('diaryComment/comment', array('comment' => $comment, 'diary' => $diary));
+}
+
+op_include_list('commentList', $list, array('border' => true));
+?>
 
 <?php if ($pager->haveToPaginate()): ?>
-<hr>
 <center>
 <?php if ($pager->hasOlderPage()): ?><?php echo link_to(__('Older'), '@diary_show?id='.$diary->id.'&page='.$pager->getOlderPage().'&order='.$order) ?><?php endif; ?>
 <?php if ($pager->hasNewerPage()): ?> <?php echo link_to(__('Newer'), '@diary_show?id='.$diary->id.'&page='.$pager->getNewerPage().'&order='.$order) ?><?php endif; ?>
