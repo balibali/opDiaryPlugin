b9e746fd366e02c8a4bc4bd591988ea425120d61
77web <info@77-web.com>
2011-01-16 14:57:27 +0900

    add message to register mobile address in newSucceess.php if the user has no mobile e-mail setting. without registration of mobile address, e-mail posting always fail. for opMailRoute class does not allow empty mobile address.

--- apps/mobile_frontend/modules/diary/templates/newSuccess.php
+++ apps/mobile_frontend/modules/diary/templates/newSuccess.php
@@ -3,8 +3,12 @@ op_mobile_page_title(__('Post a diary'));
 ?>
 
 <?php if (Doctrine::getTable('SnsConfig')->get('op_diary_plugin_use_email_post', true)): ?>
+<?php if('' != $sf_user->getMember()->getConfig('mobile_address')): ?>
 [i:106]<?php echo op_mail_to('mail_diary_create', array(), __('Post via E-mail')) ?><br>
 <?php echo __('You can attach photo files to e-mail.') ?><br>
+<?php else: ?>
+<?php echo __('To attach a photo file, please register your mobile address.') ?>(<a href="<?php echo url_for('member_config', array('category'=>'mobileAddress')); ?>"><?php echo __('Mobile E-mail Address Configuration'); ?></a>)<br>
+<?php endif; ?>
 <?php endif; ?>
 
 <?php
