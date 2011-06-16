6967f2786d6054674cf9401cc0320ea5a9b3afd1
nise_nabe <nise.nabe@gmail.com>
2011-06-16 21:06:44 +0900

    fixxed wrong displayed word (fixed #1776)

--- apps/mobile_frontend/modules/diary/templates/_noticeUnreadDiaryComment.php
+++ apps/mobile_frontend/modules/diary/templates/_noticeUnreadDiaryComment.php
@@ -1,4 +1,6 @@
 <?php if ($count): ?>
-<font color="#FF0000"><?php echo format_number_choice('[1]1 diary has new comments!|(1,Inf]%1% diaries have new comments!', array('%1%' => $count), $count) ?></font>
-<?php echo link_to(__('Read diary'), '@diary_show?id='.$diary->id) ?><br>
+<font color="red"><?php echo format_number_choice('[1]1 diary has new comments|(1,Inf]%1% diaries have new comments', array('%1%' => $count), $count) ?>
+&nbsp;
+<?php echo link_to(__('Read comment'), '@diary_show?id='.$diary->id) ?><br>
+</font>
 <?php endif; ?>
