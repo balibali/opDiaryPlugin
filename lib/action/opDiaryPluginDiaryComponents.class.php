8622c2c4cf26f1d179c8b0ceaa4eb9ed557a1b67
mtakai <m.takai@tejimaya.com>
2011-09-08 21:30:02 +0900

    (fixes #2354) removed dailyNews gadget (reverts commit:dcb0853c)

--- lib/action/opDiaryPluginDiaryComponents.class.php
+++ lib/action/opDiaryPluginDiaryComponents.class.php
@@ -50,34 +50,6 @@ class opDiaryPluginDiaryComponents extends sfComponents
     }
   }
 
-  public function executeDailyNews()
-  {
-    $env = 'mobile_frontend' == sfConfig::get('sf_app') ? 'mobile' : 'pc';
-    $twigEnvironment = new Twig_Environment(new Twig_Loader_String());
-    $valueTpl = $twigEnvironment->loadTemplate(opDiaryPluginToolkit::getMailTemplate($env, 'diaryGagdet'));
-    $diaries = Doctrine::getTable('Diary')->getFriendDiaryList($member['id'], 5);
-
-    if (!count($diaries))
-    {
-      return sfView::NONE;
-    }
-
-    $result = array();
-    foreach ($diaries as $key => $diary)
-    {
-      $result[$key]['Member'] = $diary->Member;
-      $result[$key]['title'] = $diary->title;
-      $result[$key]['id'] = $diary->id;
-    }
-    $params = array(
-      'diaries'   => $result,
-      'count'     => count($diaries),
-      'sf_config' => sfConfig::getAll(),
-    );
-
-    $this->value = $valueTpl->render($params);
-  }
-
   protected function getSnsMemberId()
   {
     return $this->getUser()->isSNSMember() ? $this->getUser()->getMemberId() : null;
