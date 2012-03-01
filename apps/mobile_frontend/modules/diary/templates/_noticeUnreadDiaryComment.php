37ab47b860201dceccc0c797571d430924487867
watanabe <watanabe@openpne.jp>
2012-03-01 16:37:59 +0900

    give color to notice messages (fixes #2802, BP from #1776)
    BP from #1776
    commit:32ff23faf88507369d573c58596b6fe3baedd612
    commit:6967f2786d6054674cf9401cc0320ea5a9b3afd1
    commit:de6550fc020cb46b11e9fb19c2299691d3f2a646
    
    Conflicts:
    
    	i18n/messages.ja.xml

--- apps/mobile_frontend/modules/diary/templates/_noticeUnreadDiaryComment.php
+++ apps/mobile_frontend/modules/diary/templates/_noticeUnreadDiaryComment.php
@@ -1,3 +1,6 @@
 <?php if ($count): ?>
-<?php echo link_to(format_number_choice('[1]1 diary has new comments!|(1,Inf]%1% diaries have new comments!', array('%1%' => $count), $count), '@diary_show?id='.$diary->id) ?><br>
+<font color="red"><?php echo format_number_choice('[1]1 diary has new comments|(1,Inf]%1% diaries have new comments', array('%1%' => $count), $count) ?>
+&nbsp;
+<?php echo link_to(__('Read comment'), '@diary_show?id='.$diary->id) ?><br>
+</font>
 <?php endif; ?>
